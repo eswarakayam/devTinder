4a3f6e8207c334b6e856fc20f145cf18e507f636
eswarakayam <eswarjs39@gmail.com>
2025-07-18 04:49:32 +0530

    exploring get post and delete

--- src/app.js
+++ src/app.js
@@ -3,16 +3,26 @@ const express = require('express')
 const app = express()
 const port = 7777
 
-app.use('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.use('/test', (req, res) => {
-  res.send('Hello World! from test')
+app.get('/user',(req,res) => {
+  res.send('{firstname:"eswar",lastname:"reddy"}');
 })
- app.use('/hello', (req, res) => {
-  res.send('Hello World! hello')
-}) 
+app.post('/user',(req,res)=>{
+  res.send("Data saved successfully");
+})
+app.delete('/user',(req,res) => {
+  res.send("Deleted successfully");
+})
+app.put('/user',(req,res)=>{
+  res.send("Entire Data updated successfully");
+})
+app.patch('/user',(req,res) => {
+  res.send("Give data is modified successfully");
+})
+// app.use('/test', (req, res) => {
+//   res.send('Hello World! from test')
+// })
+ 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
