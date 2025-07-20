2d3c2faa1efd011af5715ba801ec3718e75f3ae2
eswarakayam <eswarjs39@gmail.com>
2025-07-21 04:28:09 +0530

    multiple routes

--- src/app.js
+++ src/app.js
@@ -3,26 +3,30 @@ const express = require('express')
 const app = express()
 const port = 7777
 
+// /ac, //abc
+app.use("/user", (req,res,next) => {
+  console.log("this is Request 1");
+  next();
+ // res.send('Route1');
+  
+},
+(req,res,next) => {
+  console.log("this is Handler 2");
+  //res.send("Route2");
+  next();
+},(req,res,next) => {
+  console.log("this is Handler 3");
+ // res.send("Route3");
+  next();
+},[(req,res,next) => {
+  console.log("this is Handler 4");
+  //res.send("Route4");
+  next();
+},(req,res,next) => {
+  console.log("this is Handler 5");
+  res.send("Route5");
+}])
 
-app.get('/user',(req,res) => {
-  res.send('{firstname:"eswar",lastname:"reddy"}');
-})
-app.post('/user',(req,res)=>{
-  res.send("Data saved successfully");
-})
-app.delete('/user',(req,res) => {
-  res.send("Deleted successfully");
-})
-app.put('/user',(req,res)=>{
-  res.send("Entire Data updated successfully");
-})
-app.patch('/user',(req,res) => {
-  res.send("Give data is modified successfully");
-})
-// app.use('/test', (req, res) => {
-//   res.send('Hello World! from test')
-// })
- 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
