41560cdd987402d14af91676d2b51e15f382b1e5
eswarakayam <eswarjs39@gmail.com>
2025-07-21 11:47:13 +0530

    writing middle wares

--- src/app.js
+++ src/app.js
@@ -1,31 +1,26 @@
 console.log("Starting a new project!");
-const express = require('express')
-const app = express()
-const port = 7777
+const express = require('express');
+const app = express();
+const {adminAuth,userAuth } = require('./middlewares/auth');
+const port = 7777;
 
-// /ac, //abc
-app.use("/user", (req,res,next) => {
-  console.log("this is Request 1");
-  next();
- // res.send('Route1');
-  
-},
-(req,res,next) => {
-  console.log("this is Handler 2");
-  //res.send("Route2");
-  next();
-},(req,res,next) => {
-  console.log("this is Handler 3");
- // res.send("Route3");
-  next();
-},[(req,res,next) => {
-  console.log("this is Handler 4");
-  //res.send("Route4");
-  next();
-},(req,res,next) => {
-  console.log("this is Handler 5");
-  res.send("Route5");
-}])
+app.use("/admin", adminAuth);
+
+app.post("/user/login", (req,res)=>{
+  res.send("user logged in successfully!");
+});
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("User Data Sent");
+});
+
+app.get('/admin/getAllData',(req,res,next) => { 
+  console.log("admin get all data route 2 nd level");
+  res.send("send admin data");
+})
+app.get('/admin/deleteUser',(req,res,next) => {  
+  res.send("Deleted a user");
+})
 
 
 app.listen(port, () => {
